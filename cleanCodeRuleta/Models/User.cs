67003652309b530800861f0a1aea1fe951560e65
felipeeco <contacto@felipemoreno.co>
2020-10-29 19:53:55 -0500

    I created Data base

---
+++ cleanCodeRuleta/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cleanCodeRuleta.Models
+{
+    public partial class User
+    {
+        public int Id { get; set; }
+        public DateTime? DateCreated { get; set; }
+        public int? Number { get; set; }
+        public int? Money { get; set; }
+    }
+}
---
+++ cleanCodeRuleta/Models/cleanCodeRuletaDBContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace cleanCodeRuleta.Models
+{
+    public partial class cleanCodeRuletaDBContext : DbContext
+    {
+        public cleanCodeRuletaDBContext()
+        {
+        }
+
+        public cleanCodeRuletaDBContext(DbContextOptions<cleanCodeRuletaDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> User { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("server=DESKTOP-R3CKBCH;database=cleanCodeRuletaDB;Integrated Security=true");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.DateCreated).HasColumnType("datetime");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
