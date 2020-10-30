3460adf59fd219e7b216244ad35b53f0b4bb6c89
felipeeco <contacto@felipemoreno.co>
2020-10-29 20:21:00 -0500

    making controller

---
+++ cleanCodeRuleta/Classes/UserClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cleanCodeRuleta.Classes
+{
+	public class UserClass
+	{
+
+		public int id { get; set; }
+		public DateTime dataCreated { get; set; }
+		public int number { get; set; }
+		public int money { get; set; }
+		
+	}
+}
---
+++ cleanCodeRuleta/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using cleanCodeRuleta.Classes;
+using cleanCodeRuleta.Models;
+using System.Transactions;
+
+namespace cleanCodeRuleta.Controllers
+{
+	[ApiController]
+	public class UserController : Controller
+	{
+		[HttpPost]
+		[Route("usuario/ruleta")]
+		public UserClass Ruleta([FromBody] UserClass userClass)
+		{
+			try
+			{
+				using (cleanCodeRuletaDBContext db = new cleanCodeRuletaDBContext())
+				{
+					using (var transaccion = new TransactionScope())
+					{
+						User UserModel = new User();
+						UserModel.DateCreated = DateTime.Now;
+						UserModel.Number = userClass.number;
+						UserModel.Money = userClass.money;
+
+					}
+				}
+			}
+			catch (Exception)
+			{
+
+				throw;
+			}
+		}
+	}
+}
